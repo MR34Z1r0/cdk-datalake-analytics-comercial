0f13b327d95c9a82440cefd13fd093c1b7c829d2
Juan Alvarez <jmalvarez010902@gmail.com>
2025-05-30 10:37:07 -0500

    commit5

--- artifacts/aws-glue/code/domain/m_clasificacion_cliente.py
+++ artifacts/aws-glue/code/domain/m_clasificacion_cliente.py
@@ -13,18 +13,15 @@ try:
     m_subgiro_visibilidad = spark_controller.read_table(data_paths.APDAYC, "m_subgiro", cod_pais=cod_pais)
     m_giro_visibilidad = spark_controller.read_table(data_paths.APDAYC, "m_giro", cod_pais=cod_pais)
     m_compania = spark_controller.read_table(data_paths.APDAYC, "m_compania", cod_pais=cod_pais)
-    m_pais = spark_controller.read_table(data_paths.APDAYC, "m_pais", cod_pais=cod_pais, have_principal=True)
-
-    conf_origen_dom = spark_controller.read_table(data_paths.DOMAIN, "conf_origen")
+    m_pais = spark_controller.read_table(data_paths.APDAYC, "m_pais", cod_pais=cod_pais, have_principal=True) 
 
     target_table_name = "m_clasificacion_cliente"
 
 except Exception as e:
     raise
 
-# CREATE desde BIG_MAGIC
 try:
-    tmp4 = (
+    df_subgiro = (
         i_relacion_consumo.alias("irc")
         .join(m_compania.alias("mc"), col("mc.cod_compania") == col("irc.cod_compania"), "inner")
         .join(m_pais.alias("mp"), col("mc.cod_pais") == col("mp.cod_pais"), "inner")
@@ -35,45 +32,19 @@ try:
         )
         .where(col("mp.id_pais").isin(cod_pais))
         .select(
-            col("mp.id_pais").alias("id_pais"),
-            col("irc.cod_compania"),
-            col("irc.cod_giro"),
+            col("mp.id_pais").alias("id_pais"), 
             concat(trim(col("irc.cod_compania")), lit("|"), lit("SG"), lit("|"), trim(col("irc.cod_subgiro"))).alias("id_clasificacion_cliente"),
+            concat(trim(col("irc.cod_compania")), lit("|"), lit("GR"), lit("|"), trim(col("irc.cod_giro"))).alias("id_clasificacion_cliente_padre"),
             col("irc.cod_subgiro").alias("cod_clasificacion_cliente"),
-            col("mgv.desc_subgiro"),
+            col("mgv.desc_subgiro").alias("nomb_clasificacion_cliente"),
             lit("Subgiro").alias("cod_tipo_clasificacion_cliente"),
-            col("mgv.es_activo"),
+            col("mgv.es_activo").alias("estado"),
             current_date().alias("fecha_creacion"),
             current_date().alias("fecha_modificacion"),
         )
-        .groupby(
-            col("id_pais"),
-            col("id_clasificacion_cliente"),
-            col("cod_clasificacion_cliente"),
-            col("irc.cod_compania"),
-            col("mgv.desc_subgiro"),
-            col("mgv.es_activo"),
-            col("cod_tipo_clasificacion_cliente"),
-            col("fecha_creacion"),
-            col("fecha_modificacion"),
-        )
-        .agg(
-            concat(trim(col("irc.cod_compania")), lit("|"), lit("GR"), lit("|"), trim(max(col("irc.cod_giro")))).alias("id_clasificacion_cliente_padre")
-        )
-        .select(
-            col("id_pais"),
-            col("id_clasificacion_cliente"),
-            col("id_clasificacion_cliente_padre"),
-            col("cod_clasificacion_cliente"),
-            col("mgv.desc_subgiro").alias("nomb_clasificacion_cliente"),
-            col("cod_tipo_clasificacion_cliente"),
-            col("mgv.es_activo").alias("estado"),
-            col("fecha_creacion"),
-            col("fecha_modificacion"),
-        )
     )
 
-    tmp5 = (
+    df_giro = (
         i_relacion_consumo.alias("irc")
         .join(m_compania.alias("mc"), col("mc.cod_compania") == col("irc.cod_compania"), "inner")
         .join(m_pais.alias("mp"), col("mc.cod_pais") == col("mp.cod_pais"), "inner")
@@ -84,45 +55,19 @@ try:
         )
         .where(col("mp.id_pais").isin(cod_pais))
         .select(
-            col("mp.id_pais").alias("id_pais"),
-            col("irc.cod_compania"),
-            col("irc.cod_canal"),
+            col("mp.id_pais").alias("id_pais"), 
             concat(trim(col("irc.cod_compania")), lit("|"), lit("GR"), lit("|"), trim(col("irc.cod_giro"))).alias("id_clasificacion_cliente"),
+            concat(trim(col("irc.cod_compania")), lit("|"), lit("CN"), lit("|"), trim(col("irc.cod_canal"))).alias("id_clasificacion_cliente_padre"),
             col("irc.cod_giro").alias("cod_clasificacion_cliente"),
-            col("mgv.desc_giro"),
+            col("mgv.desc_giro").alias("nomb_clasificacion_cliente"),
             lit("Giro").alias("cod_tipo_clasificacion_cliente"),
-            col("mgv.es_activo"),
+            col("mgv.es_activo").alias("estado"),
             current_date().alias("fecha_creacion"),
             current_date().alias("fecha_modificacion"),
         )
-        .groupby(
-            col("id_pais"),
-            col("id_clasificacion_cliente"),
-            col("cod_clasificacion_cliente"),
-            col("irc.cod_compania"),
-            col("mgv.desc_giro"),
-            col("mgv.es_activo"),
-            col("cod_tipo_clasificacion_cliente"),
-            col("fecha_creacion"),
-            col("fecha_modificacion"),
-        )
-        .agg(
-            concat(trim(col("irc.cod_compania")), lit("|"), lit("CN"), lit("|"), trim(max(col("irc.cod_canal")))).alias("id_clasificacion_cliente_padre")
-        )
-        .select(
-            col("id_pais"),
-            col("id_clasificacion_cliente"),
-            col("id_clasificacion_cliente_padre"),
-            col("cod_clasificacion_cliente"),
-            col("mgv.desc_giro").alias("nomb_clasificacion_cliente"),
-            col("cod_tipo_clasificacion_cliente"),
-            col("mgv.es_activo").alias("estado"),
-            col("fecha_creacion"),
-            col("fecha_modificacion"),
-        )
     )
 
-    tmp6 = (
+    df_canal = (
         i_relacion_consumo.alias("irc")
         .join(m_compania.alias("mc"), col("mc.cod_compania") == col("irc.cod_compania"), "inner")
         .join(m_pais.alias("mp"), col("mc.cod_pais") == col("mp.cod_pais"), "inner")
@@ -142,48 +87,14 @@ try:
             current_date().alias("fecha_creacion"),
             current_date().alias("fecha_modificacion"),
         )
-        .distinct()
-        .select(
-            col("id_pais"),
-            col("id_clasificacion_cliente"),
-            col("id_clasificacion_cliente_padre"),
-            col("cod_clasificacion_cliente"),
-            col("nomb_clasificacion_cliente"),
-            col("cod_tipo_clasificacion_cliente"),
-            col("estado"),
-            col("fecha_creacion"),
-            col("fecha_modificacion"),
-        )
-    )  
-
-    tmp_dominio_m_clasificacion_cliente_bmagic = tmp4.union(tmp5).union(tmp6).distinct()
-
-    tmp_result = (
-        tmp_dominio_m_clasificacion_cliente_bmagic.alias("ccm")
-        .join(
-            conf_origen_dom.alias("co"),
-            (col("co.id_pais") == col("ccm.id_pais"))
-            & (col("co.nombre_tabla") == "m_clasificacion_cliente")
-            & (col("co.nombre_origen") == "bigmagic"),
-            "inner",
-        )
-        .where(col("ccm.id_pais").isin(cod_pais))
-        .select(
-            col("ccm.id_pais"),
-            col("ccm.id_clasificacion_cliente"),
-            col("ccm.id_clasificacion_cliente_padre"),
-            col("ccm.cod_clasificacion_cliente"),
-            col("ccm.nomb_clasificacion_cliente"),
-            col("ccm.cod_tipo_clasificacion_cliente"),
-            col("ccm.estado"),
-            col("ccm.fecha_creacion"),
-            col("ccm.fecha_modificacion"),
-        )
     )
 
-    tmp_dominio_m_clasificacion_cliente = (
-        tmp_result
-        .distinct()
+    df_subgiro = df_subgiro.distinct()
+    df_giro = df_giro.distinct()
+    df_canal = df_canal.distinct()
+
+    df_m_clasificacion_cliente = (
+        df_subgiro.union(df_giro).union(df_canal)
         .select(
             col("id_pais").cast(StringType()).alias("id_pais"),
             col("id_clasificacion_cliente").cast(StringType()).alias("id_clasificacion_cliente"),
@@ -200,7 +111,7 @@ try:
     id_columns = ["id_clasificacion_cliente"]
     partition_columns_array = ["id_pais"]
     logger.info(f"starting upsert of {target_table_name}")
-    spark_controller.upsert(tmp_dominio_m_clasificacion_cliente, data_paths.DOMAIN, target_table_name, id_columns, partition_columns_array)
+    spark_controller.upsert(df_m_clasificacion_cliente, data_paths.DOMAIN, target_table_name, id_columns, partition_columns_array)
 
 except Exception as e:
     logger.error(e)
